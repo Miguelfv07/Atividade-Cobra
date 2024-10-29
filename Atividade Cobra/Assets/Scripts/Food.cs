394598829185f962e15c0fac4a9b6d74be8a6fc6
Miguelfv07 <fagundesmiguel60@gmail.com>
2024-10-29 15:58:20 -0300

    Declaração de variaveis

---
+++ Atividade Cobra/Assets/Scripts/Food.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public Transform foodPrefab;
+    public Snake snake;
+    private Transform currentFood;
+}
